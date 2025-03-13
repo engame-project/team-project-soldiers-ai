f95dc4302efa7b2a48731bbb23d310525ba68fb4
Md. Hanif Raihan <hanifraihan561@gmail.com>
2025-03-13 14:22:41 +0600

    first commit

---
+++ Client/src/main.jsx
@@ -0,0 +1,31 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import MainLayout from './Components/MainLayout/MainLayout';
+import Home from './Components/Home/Home';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: 'about',
+        element: <h3>i am about</h3>
+      }
+    ]
+  },
+]);
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
