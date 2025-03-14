1da21314aa344803b041c7d26997ec199dc2d2b8
Durjoy96 <mddurjoy853@gmail.com>
2025-03-14 13:00:56 +0600

    refactor Navbar component and add Pricing page

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,31 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import MainLayout from './Components/MainLayout/MainLayout';
-import Home from './Components/Home/Home';
-
+import MainLayout from "./Components/MainLayout/MainLayout";
+import Home from "./Components/Home/Home";
+import Pricing from "./pages/pricing/Pricing";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <MainLayout></MainLayout>,
     children: [
       {
-        path: '/',
-        element: <Home></Home>
+        path: "/",
+        element: <Home></Home>,
       },
       {
-        path: 'about',
-        element: <h3>i am about</h3>
-      }
-    ]
+        path: "about",
+        element: <h3>i am about</h3>,
+      },
+      {
+        path: "/pricing",
+        element: <Pricing />,
+      },
+    ],
   },
 ]);
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-     <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ Client/src/pages/pricing/Pricing.jsx
@@ -0,0 +1,9 @@
+const Pricing = () => {
+  return (
+    <>
+      <h1>pricing</h1>
+    </>
+  );
+};
+
+export default Pricing;
